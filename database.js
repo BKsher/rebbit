1b4837cce1ae108c5643d1dcf7a8e863f17eb05e
BKsher <derfer2003@gmail.com>
2023-12-19 19:06:37 +0200

    Configure JawsDB MySQL

--- database.js
+++ database.js
@@ -1,19 +1,33 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: '127.0.0.1', // Hostname without the port number
-  port: 3306, // Specify the port number separately, if not the default
-  user: 'root', // your database username
-  password: '240303vova', // your database password
-  database: 'mydb' // your database name
-});
+// Check if running on Heroku with JawsDB
+if (process.env.JAWSDB_URL) {
+    // Use JawsDB URL
+    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection.connect(error => {
+        if (error) {
+            console.error('Error connecting to JawsDB MySQL:', error);
+            return;
+        }
+        console.log('Connected to JawsDB MySQL database');
+    });
+} else {
+    // Use local database configuration
+    const connection = mysql.createConnection({
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: '240303vova',
+        database: 'mydb'
+    });
 
-connection.connect(error => {
-  if (error) {
-    console.error('Error connecting to the database:', error);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
+    connection.connect(error => {
+        if (error) {
+            console.error('Error connecting to the local database:', error);
+            return;
+        }
+        console.log('Connected to local MySQL database');
+    });
+}
 
 module.exports = connection;
