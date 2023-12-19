c2a26b2b6c33f50fedf63dc2137856fe157dd926
BKsher <derfer2003@gmail.com>
2023-12-19 19:11:34 +0200

    Fix database connection scoping

--- database.js
+++ database.js
@@ -1,33 +1,27 @@
 const mysql = require('mysql2');
+let connection;
 
-// Check if running on Heroku with JawsDB
 if (process.env.JAWSDB_URL) {
     // Use JawsDB URL
-    const connection = mysql.createConnection(process.env.JAWSDB_URL);
-    connection.connect(error => {
-        if (error) {
-            console.error('Error connecting to JawsDB MySQL:', error);
-            return;
-        }
-        console.log('Connected to JawsDB MySQL database');
-    });
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     // Use local database configuration
-    const connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host: '127.0.0.1',
         port: 3306,
         user: 'root',
         password: '240303vova',
         database: 'mydb'
     });
-
-    connection.connect(error => {
-        if (error) {
-            console.error('Error connecting to the local database:', error);
-            return;
-        }
-        console.log('Connected to local MySQL database');
-    });
 }
 
+connection.connect(error => {
+    if (error) {
+        console.error('Error connecting to the database:', error);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
 module.exports = connection;
+
