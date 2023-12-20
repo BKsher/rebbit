dc049c39b13626cdd4a1025244b1255646be5456
BKsher <derfer2003@gmail.com>
2023-12-20 18:12:44 +0200

    updated posts and comments

--- comments.js
+++ comments.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./database'); 
+const authenticate = require('./auth/authenticate');
+const db = require('./database');
 
-// Endpoint to add a comment to a post
-router.post('/:postId', (req, res) => {
+// Endpoint to add a comment to a post (Authenticated users)
+router.post('/:postId', authenticate, (req, res) => {
   const { postId } = req.params;
-  const { userId, content } = req.body;
+  const userId = req.user.id; // Assuming the user ID is stored in req.user by your authentication middleware
+  const { content } = req.body;
 
-  if (!content || !userId) {
-    return res.status(400).send('Content and user ID are required');
+  if (!content) {
+    return res.status(400).send('Content is required');
   }
 
   const query = 'INSERT INTO Comments (post_id, user_id, content, comment_date) VALUES (?, ?, ?, NOW())';
@@ -21,18 +23,57 @@ router.post('/:postId', (req, res) => {
   });
 });
 
-// Endpoint to get all comments of a post
+// Endpoint to get all comments of a post (Accessible to everyone)
 router.get('/:postId', (req, res) => {
-    const { postId } = req.params;
-  
-    const query = 'SELECT * FROM Comments WHERE post_id = ?';
-    db.query(query, [postId], (err, results) => {
-      if (err) {
-        console.error('Error fetching comments:', err);
-        return res.status(500).send('Server error while retrieving comments');
-      }
-      res.json(results);
-    });
+  const { postId } = req.params;
+
+  const query = 'SELECT * FROM Comments WHERE post_id = ?';
+  db.query(query, [postId], (err, results) => {
+    if (err) {
+      console.error('Error fetching comments:', err);
+      return res.status(500).send('Server error while retrieving comments');
+    }
+    res.json(results);
   });
+});
+
+// Endpoint to update a comment (Authenticated users)
+router.put('/:commentId', authenticate, (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    return res.status(400).send('Content is required');
+  }
+
+  const query = 'UPDATE Comments SET content = ? WHERE comment_id = ?';
+  db.query(query, [content, commentId], (err, result) => {
+    if (err) {
+      console.error('Error updating comment:', err);
+      return res.status(500).send('Server error during comment update');
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).send('Comment not found');
+    }
+    res.send('Comment updated successfully');
+  });
+});
+
+// Endpoint to delete a comment (Authenticated users)
+router.delete('/:commentId', authenticate, (req, res) => {
+  const { commentId } = req.params;
+
+  const query = 'DELETE FROM Comments WHERE comment_id = ?';
+  db.query(query, [commentId], (err, result) => {
+    if (err) {
+      console.error('Error deleting comment:', err);
+      return res.status(500).send('Server error during comment deletion');
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).send('Comment not found');
+    }
+    res.send('Comment deleted successfully');
+  });
+});
 
 module.exports = router;
